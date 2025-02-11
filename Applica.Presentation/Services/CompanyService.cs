1a9802a4f9501571d6a1596a8c8e33df85838128
Johannes Breitfeld <jbvbreitfeld@gmail.com>
2025-02-11 13:08:24 +0100

    Wrapped logic of Update method in an if/else statement so that changes always will be saved to DB without code repetition.

--- Applica.Presentation/Services/CompanyService.cs
+++ Applica.Presentation/Services/CompanyService.cs
@@ -95,22 +95,22 @@ public class CompanyService
         {
             var existingCompany = await context.Companies.FindAsync(company.Id);
 
-            if(existingCompany is null)
+            if (existingCompany is null)
             {
                 await AddAsync(company);
-                return;
             }
+            else
+            {
+                var mapped = MapToEntity(company);
 
-            var mapped = MapToEntity(company);
+                existingCompany.Name = mapped.Name;
+                existingCompany.Url = mapped.Url;
+                existingCompany.Comments = mapped.Comments;
+                existingCompany.Activities = mapped.Activities;
+                existingCompany.ContactPeople = mapped.ContactPeople;
 
-            existingCompany.Name = mapped.Name;
-            existingCompany.Url = mapped.Url;
-            existingCompany.Comments = mapped.Comments;
-            existingCompany.Activities = mapped.Activities;
-            existingCompany.ContactPeople = mapped.ContactPeople;
-
-            await context.SaveChangesAsync();
-            
+            }
+             await context.SaveChangesAsync();
         }
     }
 
